d5b40ea31566fc7c4bbb81a0e8d03a97700d7a31
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-07 23:25:29 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BetterFirebaseDatabaseCodable
+
+A description of this package.
---
+++ Sources/BetterFirebaseDatabaseCodable/BetterFirebaseDatabaseCodable.swift
@@ -0,0 +1,6 @@
+public struct BetterFirebaseDatabaseCodable {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
