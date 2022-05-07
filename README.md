8ea4ff55eb0406ec9615561babab8c26486a2a80
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-07 23:56:59 +0800

    add implementation

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
-# BetterFirebaseDatabaseCodable
+# Better Firebase Database `Codable`
 
-A description of this package.
+An extension to `Firebase.DataSnapshot` that makes it work well with empty arrays.
+
+## Usage
+
+You will need to provide a `defaultValue` to use if the value does not exist on the Firebase Database.
+
+```swift
+ref.getData { data, snapshot in
+    do {
+        print(try snapshot.data(defaultValue: SampleData(values: [], moreValues: [])))
+    } catch {
+        print(error.localizedDescription)
+    }
+}
+```
+
+## What's wrong with Firebase's implementation?
+
+Firebase Database is built to store dictionaries, which means it does not handle arrays well. See [this](https://stackoverflow.com/a/48463446).
+
+Let's say you're creating a struct with empty arrays
+```swift
+struct SampleData: Codable {
+    var values: [String]
+    var moreValues: [String]
+}
+```
+
+An empty array gets treated as if it doesn't exist by the encoder and this will lead to strange situations like this:
+
+Writing data with empty arrays to FIrebase using the `Codable` implementation:
+```swift
+try ref.setValue(from: SampleData(value: [], moreValue: []))
+try ref.setValue(from: SampleData(value: ["potato"], moreValue: []))
+try ref.setValue(from: SampleData(value: [], moreValue: ["tomato"]))
+```
+
+Retrieving data (an error is thrown):
+```swift
+ref.getData { data, snapshot in
+    do {
+        print(try snapshot.data(as: SampleData.self))
+    } catch {
+        print(error.localizedDescription)
+    }
+}
+```
+
+## Why?
+I wanted to store arrays in Firebase, and naturally, I thought
+
+> if I use the same encoder, and the same decoder, I will definitely get the same value back. I mean, it would be ridiculous for that not to be the case. 
+
+I spent an hour debugging and [submitting an issue](https://github.com/firebase/firebase-ios-sdk/issues/9742) to learn about Firebase's aversion to arrays.
+
+So here we are. This package provides a way to provide default values that can substitute missing values when decoding data from Firebase Database.
--- Sources/BetterFirebaseDatabaseCodable/BetterFirebaseDatabaseCodable.swift
+++ Sources/BetterFirebaseDatabaseCodable/BetterFirebaseDatabaseCodable.swift
@@ -1,6 +1,83 @@
-public struct BetterFirebaseDatabaseCodable {
-    public private(set) var text = "Hello, World!"
+import FirebaseDatabaseSwift
+import Firebase
 
-    public init() {
+public extension Firebase.DataSnapshot {
+    /// Retrieves the value of a snapshot and converts it to an instance of
+    /// caller-specified type.
+    ///
+    /// Similar to Firebase's original implementation, however, this implementation
+    /// will work well with empty arrays and will not require all of them be converted to optionals.
+    ///
+    /// **Usage**
+    ///
+    /// You will need to provide a `defaultValue` to use if the value does not exist on the Firebase Database.
+    ///
+    /// ```swift
+    /// ref.getData { data, snapshot in
+    ///     do {
+    ///         print(try snapshot.data(defaultValue: SampleData(values: [], moreValues: [])))
+    ///     } catch {
+    ///         print(error.localizedDescription)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// **What's wrong with Firebase's implementation?**
+    ///
+    /// Firebase Database is built to store dictionaries, which means it does not handle arrays well. See [this](https://stackoverflow.com/a/48463446).
+    ///
+    /// Let's say you're creating a struct with empty arrays
+    /// ```swift
+    /// struct SampleData: Codable {
+    ///     var values: [String]
+    ///     var moreValues: [String]
+    /// }
+    /// ```
+    ///
+    /// An empty array gets treated as if it doesn't exist by the encoder and this will lead to strange situations like this:
+    ///
+    /// Writing data with empty arrays to FIrebase using the `Codable` implementation:
+    /// ```swift
+    /// try ref.setValue(from: SampleData(value: [], moreValue: []))
+    /// try ref.setValue(from: SampleData(value: ["potato"], moreValue: []))
+    /// try ref.setValue(from: SampleData(value: [], moreValue: ["tomato"]))
+    /// ```
+    ///
+    /// Retrieving data (an error is thrown):
+    /// ```swift
+    /// ref.getData { data, snapshot in
+    ///     do {
+    ///         print(try snapshot.data(as: SampleData.self))
+    ///     } catch {
+    ///         print(error.localizedDescription)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - type: The type to convert the document fields to.
+    ///   - defaultValue: A value used to replace any missing values
+    ///   - decoder: The decoder to use to convert the document. Defaults to use
+    ///   default decoder.
+    /// - Returns: Throws `DecodingError.valueNotFound`
+    ///            if the document does not exist and `T` is not an `Optional`.
+    ///
+    ///             See `Database.Decoder` for more details about the decoding process.
+    func data<T: Decodable>(defaultValue: T,
+                            decoder: Database.Decoder =
+                            Database.Decoder()) throws -> T {
+        
+        if var value = value as? [String: Any] {
+            let mirror = Mirror(reflecting: defaultValue)
+            
+            for child in mirror.children where value[child.label ?? ""] == nil {
+                guard let label = child.label else { continue }
+                value[label] = child.value
+            }
+            
+            return try decoder.decode(T.self, from: value)
+        }
+        
+        return try decoder.decode(T.self, from: value ?? NSNull())
     }
 }
